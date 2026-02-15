2838e3a8f314808f5ffa58590468f18fdc66e795
Priyanka <priyankamedisetty05@gmail.com>
2026-02-15 16:26:38 +0530

    Some changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
 import joblib
+import os
 
 app = Flask(__name__)
 
@@ -31,4 +32,5 @@ def home():
     return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
